de72e172010daafc92efc9b9fffaa4466e8e86ac
thecatkiller <apahisfree@gmail.com>
2020-08-26 08:18:39 -0500

    agregar cambios clase 01

--- Semana01_01_main.cpp
+++ Semana01_01_main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
+
+//Seccion de metodos
+bool buscarCiudad(string lista[],int n,string vbsucar);
+
+
 int main() {
 	
 	int n;
@@ -10,12 +15,31 @@ int main() {
 	
 	
 	for(int i=0;i<n;i++){
-		cout << "Ciudad " << (i+1) << ":" << endl;
+		cout << "Ciudad " << (i+1) << ": ";
 		fflush(stdin);
 		getline (cin,lista[i]);		
 	}
 	
+	string vb;
+	cout << "Ingrese la ciudad a buscar: ";
+	
+	fflush(stdin);
+    getline (cin,vb);	
+	
+	bool resp = buscarCiudad(lista,n,vb);
+	
+	cout << resp;
 	
 	
 	return 0;
 }
+
+
+bool buscarCiudad(string lista[],int n,string vbsucar){
+	for(int i=0;i<n;i++){
+		if(lista[i] == vbsucar)	
+			return true;
+	}
+	return true;
+}
+
