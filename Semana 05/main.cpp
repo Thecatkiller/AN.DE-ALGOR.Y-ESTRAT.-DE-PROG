5459cfcc2b06e0c9d60afee4c13efbc13b20cfc8
thecatkiller <apahisfree@gmail.com>
2020-09-23 08:37:32 -0500

    agregar semana 05: divide y venceras

--- Semana 05/main.cpp
+++ Semana 05/main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 using namespace std;
 
-char menuPrincipal();
+#define OPC_SUMAR       '1'
+#define OPC_PROMEDIO    '2'
+#define OPC_MAYOR       '3'
+#define OPC_SALIR       '9'
 
-int main() {
-	
-	char opc = menuPrincipal();
+char menuPrincipal();
+int sumar(int lista[],int n);
+
+int main() {	
+	char mopc; 
+	int lista[] = {15,25,13,18,14};
+	int tam = sizeof(lista) / sizeof(*lista);
+		
+		
+	do{
+		
+		mopc = menuPrincipal();
+		
+		switch(mopc){
+			case OPC_SUMAR:			
+				cout << "Sumar" << endl;
+				
+				cout << sumar(lista,tam) << endl;
+				
+				break;						
+			case OPC_PROMEDIO:
+				break;					
+			case OPC_MAYOR:
+				break;
+		}
+		
+		system("pause");
+	}while(mopc != OPC_SALIR);
 	
 	return 0;
 }
@@ -18,7 +46,14 @@ char menuPrincipal(){
 	cout << "[3] Mayor     " << endl; 
 	cout << "[9] Salir     " << endl; 
 	cout << "Seleccione: ";
-	
+	fflush(stdin);
 	char opc = getchar();
 	return opc;
 }
+
+int sumar(int lista[],int n){
+	if (n <= 0) 
+        return 0; 
+    return (sumar(lista, n - 1) + lista[n - 1]); 
+}
+
