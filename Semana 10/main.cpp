87afd2eed79ed26382a2b8e15c6e38036676857b
thecatkiller <apahisfree@gmail.com>
2020-10-27 10:37:27 -0500

    agregar semana 10

--- Semana 10/main.cpp
+++ Semana 10/main.cpp
@@ -1,7 +1,77 @@
 #include <iostream>
+#include <conio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
-int main(int argc, char** argv) {
+long  fibREC(int n);
+long  fibITE(int n);
+long* fibPD(int n);
+
+char menuPD();
+
+
+int main() {
+	char opc;
+	int n;
+	do{
+		opc = menuPD();
+		switch (opc){
+			case '1':{
+				cout << endl << "Ingrese nro:";
+				cin >> n;
+				cout << endl;
+				for(int i=0;i<=n;i++){
+					cout.width(15);
+					cout << fibREC(i);
+					if ((i+1) % 5 ==0) cout << endl; 			
+				}
+				break;
+			}	
+			case '2':{
+				cout << endl << "Ingrese nro:";
+				cin >> n;
+				cout << endl;
+				for(int i=0;i<=n;i++){
+					cout.width(15);
+					cout << fibITE(i);
+					if ((i+1) % 5 ==0) cout << endl; 			
+				}
+				break;
+			}
+		}
+		cout << endl << endl;
+		system("pause");		
+	} while(opc!='9');	
+		
 	return 0;
 }
+
+long fibREC(int n){	
+	if (n<=1) return n;
+	return fibREC(n-1) + fibREC(n-2);
+}
+
+char menuPD(){
+	system("cls");
+	setlocale(LC_ALL,"spanish");
+	fflush(stdin);
+	cout << "Menu Principal     " << endl;
+	cout << "-------------------" << endl;
+	cout <<"[1] Fibonacci REC   " << endl;
+	cout <<"[2] Fibonacci ITE   " << endl;
+	cout <<"[9] Salir           " << endl;
+	cout << "Seleccione opción: ";	
+	char op=toupper(getche());
+	return op;		
+}
+
+long fibITE(int n){
+	long a=0,b=1,c=0;
+	for (int i=0;i<n;i++){
+		c = a+b;	
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
