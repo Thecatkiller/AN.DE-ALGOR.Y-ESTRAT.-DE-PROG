5232a55879912786f9526b3a1a1bf402fcfe882a
thecatkiller <apahisfree@gmail.com>
2020-10-28 09:24:15 -0500

    agregar semana 10

--- Semana 10/main.cpp
+++ Semana 10/main.cpp
@@ -8,7 +8,7 @@ long  fibITE(int n);
 long* fibPD(int n);
 
 char menuPD();
-
+void menuCB();
 
 int main() {
 	char opc;
@@ -38,6 +38,24 @@ int main() {
 				}
 				break;
 			}
+			case '3':{
+				cout << endl << "Nro: ";
+				cin >> n;
+				cout << endl << "FIB (PD):" << endl;
+				long* lista = fibPD(n);				
+				for (int i=0;i<=n;i++){
+					cout.width(15);
+					cout << lista[i];					
+					if ((i+1)%5==0)	cout << endl;
+				}
+				cout <<endl;
+				break;
+			}
+			case '4':{
+				menuCB();
+				break;
+			}
+				
 		}
 		cout << endl << endl;
 		system("pause");		
@@ -52,14 +70,17 @@ long fibREC(int n){
 }
 
 char menuPD(){
+	system("color 02");
 	system("cls");
 	setlocale(LC_ALL,"spanish");
 	fflush(stdin);
 	cout << "Menu Principal     " << endl;
 	cout << "-------------------" << endl;
 	cout <<"[1] Fibonacci REC   " << endl;
-	cout <<"[2] Fibonacci ITE   " << endl;
-	cout <<"[9] Salir           " << endl;
+	cout <<"[2] Fibonacci ITE            " << endl;
+	cout <<"[3] Fibonacci PD             " << endl;
+	cout <<"[4] Menu Coeficiente Binomial" << endl;
+	cout <<"[9] Salir                    " << endl;
 	cout << "Seleccione opción: ";	
 	char op=toupper(getche());
 	return op;		
@@ -75,3 +96,31 @@ long fibITE(int n){
 	return a;
 }
 
+long* fibPD(int n){
+	long* lista = new long[n+1];		
+	lista[0] = 0;
+	lista[1] = 1;
+	for(int i=2;i<=n;i++){
+		lista[i] = lista[i-2] + lista[i-1];
+	}		
+	return lista;
+}
+
+void menuCB(){
+	char op;
+	
+	do{	
+		system("cls");
+		setlocale(LC_ALL,"spanish");
+		fflush(stdin);
+		cout << "Menu Coeficiente Binomial " << endl;
+		cout << "--------------------------" << endl;
+		cout <<"[1] Recursivo              " << endl;
+		cout <<"[2] Formula                " << endl;
+		cout <<"[3] CB - PD                " << endl;
+		cout <<"[9] Regresar menu principal" << endl;
+		cout << "Seleccione opción: ";	
+	    op=toupper(getche());
+			
+	} while(op!='9');
+}
